8c7f0d3b0f69f879de913b4f3a63f59f9d03af52
zeo404 <code.zaursharifov@hotmail.com>
2022-01-03 22:19:22 +0300

    response example

--- python/api/pyapi.py
+++ python/api/pyapi.py
@@ -10,6 +10,8 @@ import shutil
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Atık Tespit Sistemi Web API</h1><p>Python Flask Web API</p>'''
@@ -21,14 +23,24 @@ def api_filter():
     # kullanici_id = query_parameters.get('kullanici_id')
     global photo_byte
     photo_byte = query_parameters.get('photo_byte')
-    photo_byte = photo_byte.replace(" ","+")
+    photo_byte = photo_byte.replace(" ", "+")
     # photo_byte = photo_byte.replace("data:image/png:base64", "") + photo_byte.replace(" ","+")
     # photo_byte = photo_byte[22:]
 
     decodeimg()
     movefiles()
+    jsresponse()
+    return jsresponse()
 
-    return photo_byte
+
+def jsresponse():
+    header1 = '''<h1>Plastik kutusu</h1>'''
+    header2 = '''<h2>Pet şişe</h2>'''
+    content1 = '''<p>EPA, 2017 yılında plastik şişe üretiminin 3,7 milyon tona ulaştığını ve bunun %30'dan azının geri dönüştürüldüğünü tahmin ediyor.</p>'''
+    header3 = "Azaltmak:"
+    content2 = "Tek kullanımlık şişeler veya teneke kutular satın almak yerine kendi şişenizi getirin."
+    warning = "Atacağınız nesne <span>plastik poşetsiz, temiz, boş ve kuru olmalıdır.</span>"
+
 
 def decodeimg():
     imgdata = base64.standard_b64decode(photo_byte)
@@ -36,16 +48,15 @@ def decodeimg():
     with open(filename, 'wb') as f:
         f.write(imgdata)
 
+
 def movefiles():
     src_path = r'D:\Code\atik-tespit-sistemi\testimg.jpg'
     dst_path = r'D:\Code\atik-tespit-sistemi\python\test\testimg.jpg'
 
     shutil.move(src_path, dst_path)
 
+
 app.run()
-
-
-
 
 
 # def movefiles():
@@ -55,4 +66,3 @@ app.run()
 #     file_name = 'img.png'
 
 #     shutil.move(src_path, dst_path)
-
