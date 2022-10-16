df059a1777643ef98e8780cf7d7365752192e443
Zaur Sharifov <zaursharifov@outlook.com>
2022-10-16 16:20:11 +0300

    navigation tab ui

--- mobile/navigation/tabs.js
+++ mobile/navigation/tabs.js
@@ -1,4 +1,5 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import Home from '../screens/Home';
 import Camera from '../screens/Camera';
 import Docs from '../screens/Docs';
@@ -7,10 +8,46 @@ const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home}></Tab.Screen>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: '#0c0c0c',
+          borderRadius: 15,
+          height: 90,
+          ...style.shadow
+        },
+      }}>
+      <Tab.Screen name="Home" component={Home} 
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Text>T</Text>
+            </View>
+          )
+        }}
+      ></Tab.Screen>
       <Tab.Screen name="Camera" component={Camera}></Tab.Screen>
       <Tab.Screen name="Docs" component={Docs}></Tab.Screen>
     </Tab.Navigator>
   );
 }
+
+const style = StyleSheet.create({
+  shadow: {
+    shadowColor: '#1E5128',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
