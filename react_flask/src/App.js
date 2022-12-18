f8785a5b5abbd0d1d3d46deeace2e997ed05f690
Zaur Sharifov <zaursharifov@outlook.com>
2022-12-18 17:51:25 +0300

    proxy error

--- react_flask/src/App.js
+++ react_flask/src/App.js
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [data, set_data] = useState({
+    name: "",
+    age: "",
+    date: "",
+    programming: "",
+  });
+
+  useEffect(()=>{
+    fetch("/data").then((res)=> res.json().then((data)=>{
+      set_data({
+        name: data.Name,
+        age: data.Age,
+        date: data.Date,
+        programming: data.Programming,
+      })
+    }))
+  },[])
+
+  return <div className="App">
+    <h1>React and flask</h1>
+    <ul>
+      <li>{data.name}</li>
+      <li>{data.age}</li>
+      <li>{data.date}</li>
+      <li>{data.programming}</li>
+    </ul>
+  </div>;
 }
 
 export default App;
