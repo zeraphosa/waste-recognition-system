a7c95eac4dc9f27083e99562436cb41ebf8499de
Zaur Sharifov <zaursharifov@outlook.com>
2022-12-18 18:43:27 +0300

    server.py example

--- react_flask/flask_server/server.py
+++ react_flask/flask_server/server.py
@@ -1,3 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
+@app.route("/data")
+def data():
+    return {"data": ["photo1", "photo2", "photo3"]}
+if __name__ == "__main__":
+    app.run(debug=True)
