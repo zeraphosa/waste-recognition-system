af3ac0d5cd3fdc1026b5206e3ca4ea79ce2d0ecd
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-24 11:24:01 +0400

    .

--- mobile/flask-server/server.py
+++
@@ -1,7 +0,0 @@
-# from flask import Flask
-# app = Flask(__name__)
-# @app.route("/members")
-# def members():
-#     return {"members": ["photo1", "photo2", "photo3"]}
-# if __name__ == "__main__":
-#     app.run(debug=True)
